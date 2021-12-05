d0112e1037b878b700aa3c0613b148049bc2c735
ZhanGHanG9991 <zhanghang21@m.fudan.edu.cn>
2021-12-05 05:08:47 +0000

    add .gitignore

---
+++ recurrence/code/test01.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('/home/zh/opt/project/pycharmproject/minirocket/code')
+from minirocket import fit, transform
+from sklearn.linear_model import RidgeClassifierCV
+
+print(111)
